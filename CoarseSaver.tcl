2adc9d390a89f2824dbfdc065d65f2991ac7132d
Jafrancisco15 <147249691+Jafrancisco15@users.noreply.github.com>
2025-08-12 00:42:14 -0400

    Add files via upload

---
+++ CoarseSaver.tcl
@@ -0,0 +1,6 @@
+# AutoRescue / CoarseSaver
+# Molido más grueso
+# Preinfusión: 0.8 mL/s × 10–12 s (techo 3 bar)
+# Extracción: 1.6–1.9 mL/s × 20–24 s (techo 9 bar)
+# Final: 1.3 mL/s × 8–10 s (techo 7 bar)
+# Ratio: 1:2.4–1:2.6
